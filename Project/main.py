69ec85eb65824eb28a943036d81dc5578a425a4e
Lucas Massa <lmm@ic.ufal.br>
2020-01-29 17:06:18 -0300

    background music

--- Project/main.py
+++ Project/main.py
@@ -2,10 +2,12 @@ import pygame
 from levels_utils import *
 from menu_utils import *
 from managers_handler import *
+from pygame import mixer
 
 
 # initialize pygame
 pygame.init()
+mixer.init()
 
 # create screen
 screen = pygame.display.set_mode((DISPLAY_WIDTH,DISPLAY_HEIGHT))
@@ -15,6 +17,11 @@ pygame.display.set_caption('Will it light up?')
 
 # initialize the events manager
 manager = Menu_Manager()
+
+# background sounds
+mixer.music.load('sound/Soft-electronic-ambient-background-music.mp3')
+#mixer.music.load('sound/Electronic-ambient-background-beat.mp3')
+mixer.music.play(-1)
 
 while manager.run:
     manager.run_screen(screen)
